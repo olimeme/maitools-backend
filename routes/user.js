48684737ca4e60c911e1b23c365f31c94a3b4f70
olimeme <alibek.mombekov@mail.ru>
2023-08-10 15:39:25 +0600

    initial commit

---
+++ routes/user.js
@@ -0,0 +1,28 @@
+var express = require("express"),
+  router = express.Router(),
+  verifyToken = require("../middlewares/authJWT"),
+  { signup, signin } = require("../controllers/auth.controller.js");
+
+router.post("/register", signup, function (req, res) {});
+
+router.post("/login", signin, function (req, res) {});
+
+router.get("/hiddencontent", verifyToken, function (req, res) {
+  console.log(req.user);
+  if (!req.user) {
+    res.status(403).send({
+      message: "Invalid JWT token",
+    });
+  }
+  if (req.user.role == "admin") {
+    res.status(200).send({
+      message: "Congratulations! but there is no hidden content",
+    });
+  } else {
+    res.status(403).send({
+      message: "Unauthorised access",
+    });
+  }
+});
+
+module.exports = router;
