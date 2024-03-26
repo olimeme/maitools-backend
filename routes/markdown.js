da32b5a0fafdf1553acafc6f69f80ce84ead4be2
olimeme <olisanmomyn@proton.me>
2024-03-26 17:57:30 +0000

    Add dependencies and update markdown export functionality

--- routes/markdown.js
+++ routes/markdown.js
@@ -2,6 +2,8 @@ const express = require("express");
 const fs = require("fs");
 const verifyToken = require("../middlewares/authJWT");
 const PDFDocument = require("pdfkit");
+const { markdown } = require("markdown");
+const render = require("../helpers/markdownParser");
 
 const router = express.Router();
 
@@ -21,6 +23,7 @@ router.post("/markdown/export", verifyToken, function (req, res) {
     case "pdf":
       const pdfDoc = new PDFDocument({ bufferPages: true });
       let buffers = [];
+
       pdfDoc.on("data", buffers.push.bind(buffers));
       pdfDoc.on("end", () => {
         let pdfData = Buffer.concat(buffers);
@@ -33,7 +36,8 @@ router.post("/markdown/export", verifyToken, function (req, res) {
           .end(pdfData);
       });
 
-      pdfDoc.font("./fonts/Quicksand-Regular.ttf").fontSize(14).text(text);
+      // pdfDoc.font("./fonts/Quicksand-Regular.ttf").fontSize(14).text(parsedMd);
+      render(pdfDoc, text);
       pdfDoc.end();
       // res.download(pdfDoc, "exported-document.pdf", () => {
       //   fs.unlinkSync(pdfPath);
@@ -48,5 +52,41 @@ router.post("/markdown/export", verifyToken, function (req, res) {
       break;
   }
 });
+
+function markdownToPdfElements(doc, markdownText) {
+  const parsedMd = markdown.parse(markdownText);
+  for (let i = 1; i < parsedMd.length; i++) {
+    const element = parsedMd[i];
+    console.log(element);
+    doc.font("./fonts/Quicksand-Bold.ttf");
+    switch (element[0]) {
+      case "header":
+        switch (element[1].level) {
+          case 1:
+            doc.fontSize(24).text(element[2]);
+            break;
+          case 2:
+            doc.fontSize(20).text(element[2]);
+            break;
+          case 3:
+            doc.fontSize(18).text(element[2]);
+            break;
+          case 4:
+            doc.fontSize(16).text(element[2]);
+            break;
+          case 5:
+            doc.fontSize(14).text(element[2]);
+            break;
+          case 6:
+            doc.fontSize(12).text(element[2]);
+            break;
+        }
+        break;
+      case "link":
+        doc.fontSize(14).text(element[2]);
+        break;
+    }
+  }
+}
 
 module.exports = router;
